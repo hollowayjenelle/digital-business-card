d04effb17a6d1eaf7dfd9e23955c7fba0423218c
jholloway <jholloway@griddynamics.com>
2023-01-16 15:45:49 -0500

    add styling for Contact component; add html to Contact component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ProfileHeader from './components/ProfileHeader';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="business-card">
       <ProfileHeader />
+      <Contact />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,8 @@
 export default function Contact(){
-    
+    return(
+        <div className="contact-buttons">
+            <button className="email-button"><i className="fa fa-envelope"></i>Email</button>
+            <button className="linkedIn-button"><i className="fa fa-linkedin-square"></i>LinkedIn</button>
+        </div>
+    )
 }
