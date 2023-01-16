87d37a199ce8448186d55b3443a77311f718674a
jholloway <jholloway@griddynamics.com>
2023-01-16 15:59:31 -0500

    change App.css to add styling to About component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ProfileHeader from './components/ProfileHeader';
 import Contact from './components/Contact';
+import About from './components/About';
 
 function App() {
   return (
     <div className="business-card">
       <ProfileHeader />
       <Contact />
+      <About />
     </div>
   );
 }
