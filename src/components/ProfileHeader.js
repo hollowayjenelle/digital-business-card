2564bb3038edcbd8dc7a11391ec4cb0dcc40514d
jholloway <jholloway@griddynamics.com>
2023-01-16 14:24:12 -0500

    change ProfileHeader - add image and text; change App.js - add ProfileHeader component

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -1,3 +1,12 @@
+import profilepic from "../profilepic.jpg"
+
 export default function ProfileHeader(){
-    return
+    return(
+        <div className="profile">
+            <img src={profilepic} className="profile-pic" alt="black woman in yellow jacket"/>
+            <h3 className="full-name">Anne Walker</h3>
+            <h4 className="position">Front-end Developer</h4>
+            <h5 className="portfolio-link">annewalker.website</h5>
+        </div>
+    )
 }
